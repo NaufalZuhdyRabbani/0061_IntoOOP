7697754d9997fb4a382fe618a4b5efe1057f5839
NaufalZuhdyRabbani <naufalkzrz@gmail.com>
2024-04-22 14:46:28 +0700

    output nama

--- IntoOOP/IntoOOP.cpp
+++ IntoOOP/IntoOOP.cpp
@@ -3,4 +3,14 @@ using namespace std;
 
 class Mahasiswa {
 public: // akses modifier
+	string nama;
+	int umur;
+	string jurusan;
+
+	void output() {
+		cout << "Nama: " << nama << endl;
+		cout << "Umur: " << umur << endl;
+		cout << "Jurusan: " << jurusan << endl;
+	}
+
 };
